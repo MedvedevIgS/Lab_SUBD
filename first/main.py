640c3146acb0e6d4c232211f6f83b1465e5fbf46
igorm <igormedvedev367@gmail.com>
2022-09-18 15:25:52 +0300

    Добавил раздельную реализацию загрузки файла и вывода таблицы

--- first/main.py
+++ first/main.py
@@ -6,6 +6,7 @@ import sys
 
 class MainWindow(QMainWindow):
     db = QSqlDatabase.addDatabase('QSQLITE')
+    connectDB=False
     def __init__(self):
         super(MainWindow,self).__init__()
         uic.loadUi("FormApp.ui",self)
@@ -17,29 +18,24 @@ class MainWindow(QMainWindow):
         fname = QFileDialog.getOpenFileName(self, 'Open file', 'C:\Project1\PrSUBD\DataBase', '(*.db)')
         self.loadButDB_1.setText(fname[0])
         self.db.setDatabaseName(fname[0])
-        F_USD = QSqlTableModel()
-        F_USD.setTable('F_usd')
-        F_USD.select()
-        self.tableDB.setModel(F_USD)
+        if not self.db.open() or fname[0]=='':
+            self.connectDB = False
+        else:
+            self.connectDB = True
 
-
-    """      
-            fname=QFileDialog.getOpenFileName(self, 'Open file', 'C:\Project1\PrSUBD\DataBase', '(*.db)')
-            db=QSqlDatabase.addDatabase('QSQLITE')
-            db.setDatabaseName(fname)
+    def ShowDB(self):
+        self.OutButTable_1.setText('Работает')
+        if self.connectDB:
+            self.OutButTable_2.setText('')
             F_USD = QSqlTableModel()
             F_USD.setTable('F_usd')
             F_USD.select()
-    """
-    def ShowDB(self):
-        self.OutButTable_1.setText('Работает')
+            self.tableDB.setModel(F_USD)
+        else:
+            self.OutButTable_2.setText('Загрузите БД')
+            EmptyTab=QSqlTableModel()
+            self.tableDB.setModel(EmptyTab)
 
-        """
-        F_USD=QSqlTableModel()
-        F_USD.setTable('F_usd')
-        F_USD.select()
-        self.tableDB.setModel(F_USD)
-        """
 
 def application():
     app=QApplication(sys.argv)
