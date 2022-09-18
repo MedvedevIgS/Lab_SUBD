37111feeb936784e85775f2829116032e9c05736
igorm <igormedvedev367@gmail.com>
2022-09-18 14:45:28 +0300

    Реализация вывода таблицы и выбора файла БД с испоьзованием диалогового окна

--- first/main.py
+++ first/main.py
@@ -1,42 +1,53 @@
 from PyQt6 import uic, QtCore, QtWidgets
 from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog
-from PyQt6.uic import loadUi
 from PyQt6.QtSql import *
 
 import sys
 
 class MainWindow(QMainWindow):
+    db = QSqlDatabase.addDatabase('QSQLITE')
     def __init__(self):
         super(MainWindow,self).__init__()
-        loadUi("FormApp.ui",self)
+        uic.loadUi("FormApp.ui",self)
         self.loadButDB.clicked.connect(self.browsefiles)
         self.OutButTable.clicked.connect(self.ShowDB)
+        self.tableDB.setSortingEnabled(True)
 
     def browsefiles(self):
-        fname=QFileDialog.getOpenFileName(self, 'Open file', 'C:\Project1\PrSUBD\DataBase', '(*.db)')
-        db=QSqlDatabase.addDatabase('QSQLITE')
-        db.setDatabaseName(fname)
-        if not db.open():
-            print('Не удалось подключится к базе')
-            re
-        else:
-            print('Connection OK')
-        return db
-
-    def ShowDB(self):
-        F_USD=QSqlTableModel()
+        fname = QFileDialog.getOpenFileName(self, 'Open file', 'C:\Project1\PrSUBD\DataBase', '(*.db)')
+        self.loadButDB_1.setText(fname[0])
+        self.db.setDatabaseName(fname[0])
+        F_USD = QSqlTableModel()
         F_USD.setTable('F_usd')
         F_USD.select()
         self.tableDB.setModel(F_USD)
 
+
+    """      
+            fname=QFileDialog.getOpenFileName(self, 'Open file', 'C:\Project1\PrSUBD\DataBase', '(*.db)')
+            db=QSqlDatabase.addDatabase('QSQLITE')
+            db.setDatabaseName(fname)
+            F_USD = QSqlTableModel()
+            F_USD.setTable('F_usd')
+            F_USD.select()
+    """
+    def ShowDB(self):
+        self.OutButTable_1.setText('Работает')
+
+        """
+        F_USD=QSqlTableModel()
+        F_USD.setTable('F_usd')
+        F_USD.select()
+        self.tableDB.setModel(F_USD)
+        """
 
 def application():
     app=QApplication(sys.argv)
     window=MainWindow()
     widget=QtWidgets.QStackedWidget()
     widget.addWidget(window)
-    widget.setFixedWidth(900)
-    widget.setFixedHeight(900)
+    widget.setMinimumWidth(840)
+    widget.setMinimumHeight(660)
     widget.show()
     app.exec()
 
