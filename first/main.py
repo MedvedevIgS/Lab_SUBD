3cacb1a78abc73115186fbef2ab949f231e303d4
igorm <igormedvedev367@gmail.com>
2022-09-22 01:50:36 +0300

    Вывод таблицы реализован на кнопку "Загрузить БД". Кнопка "Вывести БД" заменена на кнопку "Применить" в дальнейшем на нее будет реализовано выведение таблицы согласно заданым условиям.

--- first/main.py
+++ first/main.py
@@ -1,19 +1,34 @@
-from PyQt6 import uic, QtCore, QtWidgets
+from PyQt6 import uic, QtCore, QtWidgets, QtGui
 from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog
 from PyQt6.QtSql import *
+import sqlite3
+import os
 
 import sys
 
 class MainWindow(QMainWindow):
-    db = QSqlDatabase.addDatabase('QSQLITE')
+    db=QSqlDatabase.addDatabase('QSQLITE')
     connectDB=False
+    filter = ''
     def __init__(self):
         super(MainWindow,self).__init__()
         uic.loadUi("FormApp.ui",self)
+        self.setWindowTitle("LR_SUBD")
         self.loadButDB.clicked.connect(self.LoadDB)
         self.BrowBut.clicked.connect(self.browsefiles)
-        self.OutButTable.clicked.connect(self.ShowDB)
+        self.filterBut.clicked.connect(self.filter_use)
         self.tableDB.setSortingEnabled(True)
+        self.RB1.setVisible(False)
+        self.RB2.setVisible(False)
+        self.RB3.setVisible(False)
+        self.RB4.setVisible(False)
+        self.RB5.setVisible(False)
+        self.RB6.setVisible(False)
+        self.RB7.setVisible(False)
+        self.RB8.setVisible(False)
+        radBut = QtWidgets.QRadioButton('radBut')
+        radBut.move(310, 100)
+
 
     def browsefiles(self):
         fname = QFileDialog.getOpenFileName(self, 'Open file', '..\DataBase\ ', '(*.db)')
@@ -21,24 +36,32 @@ class MainWindow(QMainWindow):
 
     def LoadDB(self):
         if self.BrowLine.text() != '':
-            self.loadButDB_1.setText('')
-            self.db.setDatabaseName(self.BrowLine.text())
-            if not self.db.open():
-                self.connectDB = False
-            else:
-                self.connectDB = True
-        else:
-            self.loadButDB_1.setText('Укажите путь')
-            self.connectDB = False
+            if os.path.isfile(self.BrowLine.text()):
+                self.loadButDB_1.setText('')
+                self.db.setDatabaseName(self.BrowLine.text())
+                table = QSqlTableModel()
+                table.setTable('F_USD')
+                table.setFilter(self.filter)
+                print(table.filter())
+                table.select()
+                print('table - '+table.tableName())
 
-    def ShowDB(self):
-        self.OutButTable_1.setText('Работает')
+                sqlqery = QSqlQuery()
+                # sqlqery.exec("SELECT * FROM F_USD")
+                self.tableDB.setModel(table)
+                connectDB = True
+            else:
+                self.loadButDB_1.setText('Указан неверный путь')
+                connectDB = False
+        else:
+            self.loadButDB_1.setText('Укажите Путь')
+            connectDB = False
+
+
+    def filter_use(self):
         if self.connectDB:
             self.OutButTable_2.setText('')
-            F_USD = QSqlTableModel()
-            F_USD.setTable('F_usd')
-            F_USD.select()
-            self.tableDB.setModel(F_USD)
+            filter_count = 0
         else:
             self.OutButTable_2.setText('Загрузите БД')
             EmptyTab=QSqlTableModel()
